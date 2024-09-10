db823a9110e61637b19eb47add6349760569e592
phamthimylinhhaui <phamthimylinhhaui@gmail.com>
2024-09-10 22:54:19 +0700

    lesson-5/scope: part 1

--- lesson-5-scope.js
+++ lesson-5-scope.js
@@ -4,3 +4,22 @@
 //    - Code block - Khối mã: let cost
 //    - Local block - hàm: var, function
 // -
+
+// 1, Global
+{
+  var x = 1;
+}
+console.log(x)
+
+// Khối mã
+{
+  let y = 2
+  let x = 2
+}
+// console.log(y); error
+
+// hàm
+function xy() {
+  var z = 3
+}
+// console.log(z) error
