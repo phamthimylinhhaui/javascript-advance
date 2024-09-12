c9b72ada40890ce37431dfb4e4088f77e23f80a6
phamthimylinhhaui <phamthimylinhhaui@gmail.com>
2024-09-12 23:27:48 +0700

    lesson-8/closure

--- lesson-8-closure.js
+++ lesson-8-closure.js
@@ -1,2 +1,15 @@
+// 1. Closure
+//  là 1 hàm có thể ghi nhớ nơi nó được tạo và truy cập được biến ở phạm vi bên ngoài của nó
+//
+// 2. Ứng dụng
+// - Viết code ngắn gọn hơn
+// - Biều diễn, ứng dụng tính private trong OOP
+//
+// 3. Tóm tắt
 //
 //
+//
+// 4. Lưu ý
+// - Biến được tham chiếu (refer) trong closure sẽ không được xóa khỏi bộ nhớ khi hàm cha thực thi xong
+// - Các khái niệm js nâng cao rất dễ gây nhầm lẫn
+
