d431e75c99a12d40e9b28d3c7c67d6803182f587
nấm <87581534+phamthimylinhhaui@users.noreply.github.com>
2024-09-14 11:40:10 +0700

    lesson-8/closure (#4)
    * lesson-8/closure: init
    
    * lesson-8/closure
    
    * exam 1: about closure
    
    * exam2: write shorter code
    
    * exam3: create storage
    
    * exam 3: private

---
+++ lesson-8-closure.js
@@ -0,0 +1,103 @@
+// 1. Closure
+//  là 1 hàm có thể ghi nhớ nơi nó được tạo và truy cập được biến ở phạm vi bên ngoài của nó
+//
+// 2. Ứng dụng
+// - Viết code ngắn gọn hơn
+// - Biều diễn, ứng dụng tính private trong OOP
+// => không cho phép truy cập các dl không public ra ngoài
+//
+// 3. Tóm tắt
+//
+//
+//
+// 4. Lưu ý
+// - Biến được tham chiếu (refer) trong closure sẽ không được xóa khỏi bộ nhớ khi hàm cha thực thi xong
+// - Các khái niệm js nâng cao rất dễ gây nhầm lẫn
+
+// 1.
+function createCounter() {
+    let counter = 0;
+
+    function increase() {
+        counter = counter + 1;
+        return counter;
+    }
+
+    return increase;//return function increase
+}
+
+//tao ra moi truong thuc thi moi, tham chieu den gia tri moi nhat cua counter
+const counter1 = createCounter();//gan function createCounter
+
+console.log(counter1())//1
+console.log(counter1())//2
+console.log(counter1())//3
+
+// 2. log type
+
+function createLog(namespace) {
+    function logger(message) {
+        console.log(`[${namespace}]: ${message}`)
+    }
+
+    return logger;
+}
+
+const infoLogger = createLog('Info');
+infoLogger('register cong');
+infoLogger('gui mail thanh cong');
+
+const errorLogger = createLog('Error');
+errorLogger('register that bai');
+errorLogger('gui mail that bai');
+
+// 2. creat storeage
+function createStorage(key) {
+    const store = JSON.parse(localStorage.getItem(key)) ?? {}
+    const save = () => {
+        localStorage.setItem(key, JSON.stringify(store))
+    };
+
+    const storage = {
+        get(key) {
+            return store[key];
+        },
+        set(key, value) {
+            store[key] = value;
+            save()
+        },
+        delete(key) {
+            delete store[key];
+            save();
+        }
+    }
+
+    return storage;
+}
+
+const profileSetting = createStorage('profile');
+profileSetting.set('fullname', 'linhlinh');
+profileSetting.set('age', '12');
+profileSetting.set('address', 'nam dinh');
+profileSetting.delete('address');
+console.log(profileSetting.get('fullname'));
+
+//2.exam private
+
+function createApp() {
+    const cars = [];
+
+    return {
+        add(value) {
+            cars.push(value)
+        },
+        show() {
+            console.log(cars)
+        }
+    }
+}
+const app = createApp();
+app.add('bmv');
+app.add('honda');
+app.show();
+
