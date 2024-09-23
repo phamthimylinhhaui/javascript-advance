fb94ed3cb5a1561db3aca49e8b3130707136eb8b
phamthimylinhhaui <phamthimylinhhaui@gmail.com>
2024-09-23 09:27:19 +0700

    lesson-14

---
+++ lesson-14-primitive-reference-type.js
@@ -0,0 +1,17 @@
+// Overview
+// value type and reference type
+// 1. Value type (primitive types - dữ liệu nguyên thủy )
+// - String
+// - Number
+// - Boolean
+// - BigInt
+// - Symbol
+// - undefined
+// - null
+// 2. Reference types (Non-primitive data types )
+// - Object
+// - Array
+// - Function
+// 3. Data types with functions
+// - Value types
+// - Reference types
