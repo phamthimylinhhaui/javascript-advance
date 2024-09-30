e16972cb3e9b1478f097518f802ba1db8ec6d36d
nấm <87581534+phamthimylinhhaui@users.noreply.github.com>
2024-10-01 03:11:55 +0700

    lesson-16 (#8)
    * lesson-16
    
    * lesson-16

---
+++ lesson-16-passbyvalue.js
@@ -0,0 +1,12 @@
+// Overview
+// 1.pass by value (truyen theo gia tri)
+// - sao chép giá trị của biến
+// - truyền bản sao vào trong hàm ( không làm thay đổi giá trị ngoài hàm )
+
+// 2. pass by reference ( truyen theo tham chieu)
+// - Truyền trực tiếp tham chiếu của biến vào trong hàm
+// - Làm biến ngoài hàm thay đổi khi gán lại
+
+// Js chỉ hỗ trợ pass by value
+//  - Không nhầm giữa gán lại object và gán lại thuộc tính
+
