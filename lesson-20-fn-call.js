9fd0af7f7acd8e58312a43490a0a9f0bb335b308
phamthimylinhhaui <phamthimylinhhaui@gmail.com>
2024-10-08 22:04:24 +0700

    lesson-20: overview

--- lesson-20-fn-call.js
+++ lesson-20-fn-call.js
@@ -1,1 +1,8 @@
 // Overview
+// - Là phương thức trong prototype của function constructor, phương thc này được dùng để gọi hàm và cũng có thể bind this cho hàm
+
+// - Ví dụ
+//      - goi ham voi call method
+//      - goi ham va bind this, luu y trong stric mode van co this neu duoc bind
+//      - the hien tinh ke thừa (extends) trong OOP
+//      - Mượn hàm (function borrowing), the vi du voi argument
