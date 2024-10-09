3ec9af93f17899771772402f35feed16d4ca541d
nấm <87581534+phamthimylinhhaui@users.noreply.github.com>
2024-10-09 23:09:49 +0700

    lesson-20: Fn.call() - Section 1 (#11)
    * lesson-20
    
    * lesson-20: overview
    
    * lesson-20: example section 1
    
    * lesson-20: example section 1

---
+++ lesson-20-fn-call.js
@@ -0,0 +1,44 @@
+// 'use strict';
+// Overview
+// - Là phương thức trong prototype của function constructor, phương thc này được dùng để gọi hàm và cũng có thể bind this cho hàm
+
+// - Ví dụ
+//      - goi ham voi call method
+//      - goi ham va bind this, luu y trong stric mode van co this neu duoc bind
+//      - the hien tinh ke thừa (extends) trong OOP
+//      - Mượn hàm (function borrowing), the vi du voi argument
+
+// Exam 1
+function random() {
+    console.log(Math.random());
+}
+random();
+random.call()
+
+// Exam 2: mượn hàm
+
+const teacher = {
+    name: "John",
+}
+
+const me = {
+    name: "linh",
+    getFullName() {
+        console.log(this)
+        return this.name;
+    }
+}
+
+console.log(me.getFullName())
+console.log(me.getFullName.call())// this o day la window nen k co
+console.log(me.getFullName.call(me))
+console.log(me.getFullName.call(teacher))//john
+
+this.name = 'linh global';
+
+function getName() {
+    return this.name;
+}
+
+// console.log(getName())// trong strict mode la undefine
+console.log(getName.call(this))// van ok khi chu dong rang buoc
