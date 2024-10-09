9be936555c559cb16bbab3ff25525f3924c788b5
phamthimylinhhaui <phamthimylinhhaui@gmail.com>
2024-10-09 22:54:20 +0700

    lesson-20: example section 1

--- lesson-20-fn-call.js
+++ lesson-20-fn-call.js
@@ -6,3 +6,29 @@
 //      - goi ham va bind this, luu y trong stric mode van co this neu duoc bind
 //      - the hien tinh ke thừa (extends) trong OOP
 //      - Mượn hàm (function borrowing), the vi du voi argument
+
+// Exam 1
+function random() {
+    console.log(Math.random());
+}
+random();
+random.call()
+
+// Exam 2: mượn hàm
+
+const teacher = {
+    name: "John",
+}
+
+const me = {
+    name: "linh",
+    getFullName() {
+        console.log(this)
+        return this.name;
+    }
+}
+
+console.log(me.getFullName())
+console.log(me.getFullName.call())// this o day la window nen k co
+console.log(me.getFullName.call(me))
+console.log(me.getFullName.call(teacher))//john
