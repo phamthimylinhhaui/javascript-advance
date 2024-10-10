b02cca4aae7749ca7117d4068bb105e735c67b64
phamthimylinhhaui <phamthimylinhhaui@gmail.com>
2024-10-10 21:33:09 +0700

    lesson-20-2: example

--- lesson-20-fn-call.js
+++ lesson-20-fn-call.js
@@ -42,3 +42,41 @@ function getName() {
 
 // console.log(getName())// trong strict mode la undefine
 console.log(getName.call(this))// van ok khi chu dong rang buoc
+
+// Exam 3: ke thua trong oop
+
+const Animal = function (name, weight) {
+    this.name = name;
+    this.weight = weight;
+}
+
+const Chicken = function (name, weight, legs) {
+    Animal.call(this, name, weight);// ke thua
+    this.legs = legs;
+}
+console.log(new Chicken('LINH', 32, 1));
+
+
+// Exam 4: arguments
+function logger() {
+    console.log(...arguments);
+
+    //muon for arguments ra nhung no k co phuong thuc forEach => dung ham muon voi argument
+    const cars = ['bmv', 'mec'];
+    cars.forEach(item => console.log(item));
+    //     foreach dung this tro den doi tuong truoc no
+
+    Array.prototype.forEach.call(arguments, item => console.log(item));// call bind this la argument roi thuc hien goi ham foreach
+
+//     c2
+    const arr = Array.prototype.slice.call(arguments);
+    console.log(arr)
+    arr.forEach(item => console.log(item))
+
+//     c3
+    const arr2 = Array.from(arguments)
+    console.log(arr2)
+    arr2.forEach(item => console.log(item))
+
+}
+logger(1, 2, 3)
