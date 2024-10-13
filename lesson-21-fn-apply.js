f0a8af96b036dae820c3209cdf06ec69b43b3e73
nấm <87581534+phamthimylinhhaui@users.noreply.github.com>
2024-10-14 00:05:05 +0700

    lesson-21: exam3 (#14)
    * lesson-21: exam3
    
    * lesson-21: exam3- 2

--- lesson-21-fn-apply.js
+++ lesson-21-fn-apply.js
@@ -47,6 +47,53 @@ console.log(me.isOnline)
 teacher2.goOnline.apply(me);
 console.log(me.isOnline)
 
-// exam3
+// exam3: extend
+const Animal = function (name, weight) {
+    this.name = name;
+    this.weight = weight;
+}
 
+const Parrot = function () {
+    Animal.apply(this, arguments);
+    this.speak = function () {
+        console.log('nha co khach!')
+    }
+}
 
+const parrot1 = new Parrot('vet', 300);
+parrot1.speak();
+console.log(parrot1.name)
+console.log(parrot1.weight)
+
+// So sanh
+// - Giong:
+//      - cu phap giong nhau (fn.apply, fn.bind, fn.call)
+//      - deu la các method được kết thừa từ funtion prototype (fn.bind === Function.prototype.bind)
+
+// - Khac nhau
+//      - Đối số và cách hoạt động
+/*
+- bind
+    - Trả về hàm mới với 'this' tham chiếu tới thisArg
+    - Không thực hiện gọi hàm
+    - Nếu được bind kèm arg1, arg2 thì ưu tiên các đối số này hơn
+
+    const newFn = fn.bind(thisArg, arg1, arg2, ...);
+    newFn(arg1, arg2)
+ */
+
+/*
+- call
+    - Thực hiện bind với this và thực hiện gọi hàm
+    - Nhận các đối số từ hàm gốc: arg1, arg2
+
+    fn.call(thisArg, arg1, arg2)
+ */
+
+/*
+- apply
+    - Thực hiện bind với this và thực hiện gọi hàm
+    - Nhận các đối số từ hàm gốc là dạng mảng: [arg1, arg2]
+
+    fn.apply(thisArg, [arg1, arg2])
+ */
